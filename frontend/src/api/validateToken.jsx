9400d1f5d713d8d7ae1d3f11e12de92f493f1f16
Bryan Kristofferson <bryangators510@gmail.com>
2024-03-08 11:37:16 -0500

    fixed validete token call to use new apiservice

--- frontend/src/api/validateToken.jsx
+++ frontend/src/api/validateToken.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { fetchGet } from './apiService';
 
 const ValidateToken = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -7,26 +7,14 @@ const ValidateToken = () => {
 
     useEffect(() => {
         const validateToken = async () => {
-            const apiUrl = import.meta.env.VITE_API_BASE_URL_DEVELOPMENT; // Get your API URL
-            const token = localStorage.getItem('access_token');
-
-            if (token) {
-                try {
-                    const response = await axios.get(apiUrl + '/verify_token/', {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    });
-                    setIsLoggedIn(true);
-                } catch (error) { 
-                    setIsLoggedIn(false);
-                } finally {
-                    setIsLoading(false);
-                }
-            } else {
+            try {
+                const response = await fetchGet('/verify_token/', true);
+                setIsLoggedIn(true);
+            } catch (error) { 
                 setIsLoggedIn(false);
+            } finally {
                 setIsLoading(false);
-            }
+            }            
         };
         validateToken();
     }, []);
